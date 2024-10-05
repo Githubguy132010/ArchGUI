f381a1cb546e07bbc468aca967a7645be18a5b09
Thomas Brugman <145768128+Githubguy132010@users.noreply.github.com>
2024-10-05 13:36:10 +0200

    Fix variables error.

--- profiledef.sh
+++ profiledef.sh
@@ -16,6 +16,7 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
@@ -24,8 +25,9 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/root/custom-scripts/setup.sh"]=755
-  )
+  ["/root/custom-scripts/setup.sh"]="0:0:755"
+)
+
 run_once=(
-    ["/root/custom-scripts/setup.sh"]
+  "/root/custom-scripts/setup.sh"
 )
