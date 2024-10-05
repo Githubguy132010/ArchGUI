54d2fcfe560b9b2e72397c90bfbfd54cbae83091
Thomas Brugman <145768128+Githubguy132010@users.noreply.github.com>
2024-10-05 14:13:25 +0200

    Finally create ArchGUI.

--- profiledef.sh
+++ profiledef.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
+# General ISO settings
 iso_name="archlinux"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
@@ -17,15 +18,31 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 
+# Define file permissions
 file_permissions=(
+  # Secure permission for /etc/shadow
   ["/etc/shadow"]="0:0:400"
+  
+  # Root directory permission
   ["/root"]="0:0:750"
+  
+  # Automated script (if you have one)
   ["/root/.automated_script.sh"]="0:0:755"
+  
+  # GnuPG directory for root (if used)
   ["/root/.gnupg"]="0:0:700"
+  
+  # Custom scripts permissions
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  
+  # Your custom setup script
   ["/root/custom-scripts/setup.sh"]="0:0:755"
+
+  # Install Arch Linux desktop entry
+  ["/etc/skel/Desktop/install-arch.desktop"]="0:0:755"
+
 )
 
 run_once=(
